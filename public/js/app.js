9234c48da89e7bdbe1060cd4f6e20b3ebad00cb8
Eduardo Camargo <jesc_secret@protonmail.com>
2025-03-31 16:47:01 -0300

    ok

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,14 @@
-const socket = new WebSocket("ws://192.168.20.201:8080");
+const temp = document.getElementsByTagName("span")
 
-socket.onmessage = function (event) {
-    const data = JSON.parse(event.data);
-    console.log('Temperatura recebida:', data.temperatura);
-    document.getElementById("temperatura").textContent = data.temperatura;
-};
+async function fetchData() {
+    const data = await fetch('http://localhost:3000/api/data')
+    if (data.status === 200){
+        const response = await data.json()
+        temp[0].textContent = response['value'];
+        console.log(response['value']);
+        console.log(temp[0].textContent);
+    }
+}
 
-socket.onerror = function (error) {
-    console.error("Erro na conexão WebSocket:", error);
-};
+fetchData();
+setInterval(fetchData, 5000);
--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const WebSocket = require('ws');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
-const port = 8080;
-
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(express.json());
+
+let valueAtual = "25.0";
+
+app.post('/api/data', (req, res) => {
+    if (req.body.data) {
+        valueAtual = req.body.data;
+        console.log("Nova temperatura recebida:", valueAtual);
+        res.status(200).send("Dados recebidos com sucesso");
+    } else {
+        res.status(400).send("Erro: Nenhuma temperatura foi enviada");
+    }
+});
+
+app.get('/api/data', (req, res) => {
+    res.json({ value: valueAtual });
+});
 
 app.get('/temperatura',(req, res)=>{
     res.sendFile(path.join(__dirname, '..','public', 'temperatura.html'));
 });
 
 app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname, '..','public', 'index.html'));
+   res.sendFile(path.join(__dirname, '..','public', 'index.html'));
 });
 
-wss.on('connection', (ws) =>{
-    console.log("Client´s connection",+ws.id);
-
-    ws.on('message', (message) =>{
-        wss.clients.forEach((client) =>{
-            if (client.readyState === WebSocket.OPEN){
-                client.send(message);
-            }
-        });
-    });
-
-    ws.on('close', () =>{
-        console.log("Client´s desconnection")
-    });
+app.get('/data', (req, res) => {
+    res.send("Funciona!!!");
 });
 
 
