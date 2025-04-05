f98ce4240cef00d335050f2d7f49c04c3d03393c
Eduardo Camargo <jesc_secret@protonmail.com>
2025-04-05 05:16:09 -0300

    test-time

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,20 @@
-const temp = document.getElementsByTagName("span")
+const bar = document.getElementById("waterBar");
 
 async function fetchData() {
-    const data = await fetch('http://localhost:3000/api/data')
+    const data = await fetch('http://192.168.86.7:3000/api/data')
     if (data.status === 200){
         const response = await data.json()
-        temp[0].textContent = response['value'];
-        console.log(response['value']);
-        console.log(temp[0].textContent);
+        bar.textContent = response['value'] + ' L';
+        updateBar(parseFloat(response['value']));
+    }
+}
+
+function updateBar(volume) {
+    const percentage = (parseFloat(volume) / 2) * 100; // Converte litros em porcentagem (0 a 2L)
+
+    if (volume >= 0 && volume <= 2) {
+        bar.style.height = percentage + '%';
+        bar.textContent = volume.toFixed(1) + ' L'; // Mostra com uma casa decimal
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(express.json());
 
-let valueAtual = "25.0";
+let valueAtual = "1";
 
 app.post('/api/data', (req, res) => {
-    if (req.body.data) {
-        valueAtual = req.body.data;
+    if (req.body) {
+        valueAtual = req.body.value;
         console.log("Nova temperatura recebida:", valueAtual);
         res.status(200).send("Dados recebidos com sucesso");
     } else {
@@ -26,18 +26,9 @@ app.get('/api/data', (req, res) => {
     res.json({ value: valueAtual });
 });
 
-app.get('/temperatura',(req, res)=>{
-    res.sendFile(path.join(__dirname, '..','public', 'temperatura.html'));
-});
-
 app.get('/',(req, res)=>{
    res.sendFile(path.join(__dirname, '..','public', 'index.html'));
 });
-
-app.get('/data', (req, res) => {
-    res.send("Funciona!!!");
-});
-
 
 app.listen(port, ()=>{
     console.log("Server Running at http://localhost:"+port);
